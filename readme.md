23768c5e80072942fd8273b9fb37695aaf17b461
Casres <christian@casillas.tech>
2022-04-24 16:20:25 -0400

    Testing3

--- readme.md
+++ readme.md
@@ -1,33 +1,31 @@
 
         
-        # ** Test run
-
-        ## ** License
-        MIT
-
-        ## ** Table of contents
-        - [Heading](#heading)
-        - [Heading](#heading-1)
-        - [Heading](#heading-2)
-
-        ## ** Description
-        # This is a test run
-
-        ## ** Languages
-        # JavaScript,HTML,CSS,ES6,jQuery,Bootstrap,Node,Next.js,React.js,Vue.js,Angular,Svelte,Laravel,Other
-
-        ## ** Usage
-        # This is a test run to see if this works now
-
-        ## ** Installation Instructions
-        npm
-
-        ## ** Contact Information
-        # email
-        
-        ## ** FAQ
-        # questions galore
-
-        ## ** How to contribute
-        # issues
+    ## License
+    ash
+    
+    ## Table of contents
+    - [Heading](#heading)
+    - [Heading](#heading-1)
+    - [Heading](#heading-2)
+    
+    ## Description
+    # sh
+    
+    ## Languages
+    # 
+    
+    ## Usage
+    # h
+    
+    ## Installation Instructions
+    sfh
+    
+    ## Contact Information
+    # sh
+            
+    ## FAQ
+    # wth
+    
+    ## How to contribute
+    # hjyt
     
--- utils/readme-template.js
+++ utils/readme-template.js
@@ -18,36 +18,34 @@ module.exports = readmeTemplateLayout => {
 
     return `
         
-        # **${readmeContent(name)}
-
-        ## **License
-        ${readmeContent(license)}
-
-        ## **Table of contents
-        - [Heading](#heading)
-        - [Heading](#heading-1)
-        - [Heading](#heading-2)
-
-        ## **Description
-        # ${readmeContent(description)}
-
-        ## **Languages
-        # ${readmeContent(languages)}
-
-        ## **Usage**
-        # ${readmeContent(usage)}
-
-        ## **Installation Instructions**
-        ${readmeContent(InstallationInstructions)}
-
-        ## **Contact Information**
-        # ${(readmeContent(contact))}
-        
-        ## **FAQ**
-        # ${(readmeContent(FAQ))}
-
-        ## **How to contribute**
-        # ${(readmeContent(contribute))}
+    ## License
+    ${readmeContent(license)}
+    
+    ## Table of contents
+    - [Heading](#heading)
+    - [Heading](#heading-1)
+    - [Heading](#heading-2)
+    
+    ## Description
+    # ${readmeContent(description)}
+    
+    ## Languages
+    # ${readmeContent(languages)}
+    
+    ## Usage
+    # ${readmeContent(usage)}
+    
+    ## Installation Instructions
+    ${readmeContent(InstallationInstructions)}
+    
+    ## Contact Information
+    # ${readmeContent(contact)}
+            
+    ## FAQ
+    # ${readmeContent(FAQ)}
+    
+    ## How to contribute
+    # ${readmeContent(contribute)}
     `;
 }
 
